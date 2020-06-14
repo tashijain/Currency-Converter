40821fe6a1ab99d4bacd02d2399fd384f9c7dcb2
Tashi Jain <tashjain88@gmail.com>
2020-06-14 16:47:05 -0700

    designing commit

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
