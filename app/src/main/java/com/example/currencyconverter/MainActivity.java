5179cb9d912ffefe32123fcff11e22d3a499565c
Tashi Jain <tashjain88@gmail.com>
2020-06-14 16:59:11 -0700

    converting currency INR to Dollars

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void convertCurrency(View view) {
 
+        Log.i("Info", "Button pressed");
+
+        EditText amountEditText = findViewById(R.id.amountEditText);
+
+        Log.i("Info", amountEditText.getText().toString());
+
+        double amountInRupees = Double.parseDouble(amountEditText.getText().toString());
+
+        // this is hardcoded based on 06/14/2020
+        double amountInDollars = amountInRupees * 0.013;
+
+        //String.format("%.2f", amountInDollars)
+        Toast.makeText(this, "Rs." + amountInRupees + " is $" + amountInDollars,
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
